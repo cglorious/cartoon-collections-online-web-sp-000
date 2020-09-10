8e4d7003aec85800acb780373937747fa0f9d273
Catherine Gloria <catherinegloria@gmail.com>
2020-09-10 08:55:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map do |element|
+    activate = element.capitalize
+    "#{activate}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  # calls_short = ["wind", "fire", "tree", "axe", "code"]
+  # calls_long = ["axe", "earth", "wind", "fire"]
+  long_words = array.include? do |element|
+    if element.length > 4
+      true
+    else
+      false
+    end
+  end
+
+  if long_words.include?(true)
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |food|
+    if food.include?(cheese_types)
+      #return the first element that is cheese
+      #return nil if there's no cheese
+    end
+  end
 end
