24f78489d636d6381c03e14afabd6cc59a1d391c
Catherine Gloria <catherinegloria@gmail.com>
2020-09-10 09:14:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,12 +31,13 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.map do |food|
     return food if cheese_types.include?(food)
-      #return the first element that is cheese .find
-      #return nil if there's no cheese
-      # cheddar_cheese = %w[banana cheddar sock]
-      # no_cheese = %w[ham cellphone computer]
-      # find_the_cheese(cheddar_cheese)
-      # find_the_cheese(no_cheese)
     end
   end
 end
+
+#return the first element that is cheese .find
+#return nil if there's no cheese
+# cheddar_cheese = %w[banana cheddar sock]
+# no_cheese = %w[ham cellphone computer]
+# find_the_cheese(cheddar_cheese)
+# find_the_cheese(no_cheese)
